ce9d3918509eebb885f9a02477900dd67f70285c
Ryan Dy <rdy@pivotal.io>
2015-11-11 14:53:30 -0800

    do not disable async and async generators for babel presets
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ module.exports = {
     require("babel-plugin-transform-es2015-block-scoping"),
     require("babel-plugin-transform-es2015-typeof-symbol"),
     require("babel-plugin-transform-es2015-modules-commonjs", {loose: true}),
-    [require("babel-plugin-transform-regenerator"), { async: false, asyncGenerators: false }],
+    require("babel-plugin-transform-regenerator")
   ]
 };
